4f5ed5175e486ce5a97518fd7f7f19be472c85d1
Jorge <jorgefdezamestoy@gmail.com>
2026-08-11 12:02:56 +0200

    Update info_from_blastp.py

--- scripts/ecoli_analysis/info_from_blastp.py
+++ scripts/ecoli_analysis/info_from_blastp.py
@@ -1,13 +1,14 @@
 #### Extracting information from BLASTp results ####
 
 """
-
+This script extracts and organizes results from a BLASTp search,
+as a method of functional annotation of the protein clusters.
 """
 
 
 import pandas as pd
 
-# Cargar datos
+# Load data
 cols = ["qseqid", "sseqid", "pident", "length", "qlen", "slen",
         "qcovs", "qcovhsp", "mismatch", "gapopen",
         "qstart", "qend", "sstart", "send", "evalue", "bitscore"]
@@ -19,18 +20,18 @@ blast_tsv = pd.read_csv(
 )
 
 
-# Ordenar por calidad del hit
+# Order by quality of hit
 blast_sorted = blast_tsv.sort_values(
     by=["sseqid", "evalue", "bitscore"],
     ascending=[True, True, False]
 )
 
-# Quedarse con el mejor hit por cada subject
+# Keep only the best hit per subject
 blast_best = blast_sorted.drop_duplicates(subset="sseqid", keep="first")
 
-print(len(blast_best))  # debería ser 266
+print(len(blast_best)) # just checkin'
 
-# Guardar resultado
+# Save results
 blast_best.to_csv(
     "results_panta_pipolin/blast_best_per_subject.tsv",
     sep="\t",
